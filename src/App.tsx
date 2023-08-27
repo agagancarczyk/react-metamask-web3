1788e5dd4315a11e649245537360a814d0cd12ad
Agnieszka Gancarczyk <agancarc@redhat.com>
2023-08-27 08:44:51 +0100

    added utilities, balance, chainId

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 import detectEthereumProvider from "@metamask/detect-provider";
+import { formatBalance, formatChainAsNum } from "./utils";
 
 const App = () => {
   const [hasProvider, setHasProvider] = useState<boolean | null>(null);
-  const initialState = { accounts: [] };
+  const initialState = { accounts: [], balance: "", chainId: "" };
   const [wallet, setWallet] = useState(initialState);
 
   useEffect(() => {
@@ -16,6 +17,10 @@ const App = () => {
       }
     };
 
+    const refreshChain = (chainId: any) => {
+      setWallet((wallet) => ({ ...wallet, chainId }));
+    };
+
     const getProvider = async () => {
       const provider = await detectEthereumProvider({ silent: true });
       setHasProvider(Boolean(provider));
@@ -24,17 +29,28 @@ const App = () => {
         const accounts = await window.ethereum.request({ method: "eth_accounts" });
         refreshAccounts(accounts);
         window.ethereum.on("accountsChanged", refreshAccounts);
+        window.ethereum.on("chainChanged", refreshChain);
       }
     };
 
     getProvider();
     return () => {
       window.ethereum?.removeListener("accountsChanged", refreshAccounts);
+      window.ethereum?.removeListener("chainChanged", refreshChain);
     };
   }, []);
 
   const updateWallet = async (accounts: any) => {
-    setWallet({ accounts });
+    const balance = formatBalance(
+      await window.ethereum!.request({
+        method: "eth_getBalance",
+        params: [accounts[0], "latest"],
+      })
+    );
+    const chainId = await window.ethereum!.request({
+      method: "eth_chainId",
+    });
+    setWallet({ accounts, balance, chainId });
   };
 
   const handleConnect = async () => {
@@ -52,7 +68,14 @@ const App = () => {
           Connect MetaMask
         </button>
       )}
-      {wallet.accounts.length > 0 && <div>Wallet Accounts: {wallet.accounts[0]}</div>}
+      {wallet.accounts.length > 0 && (
+        <>
+          <div>Wallet Accounts: {wallet.accounts[0]}</div>
+          <div>Wallet Balance: {wallet.balance}</div>
+          <div>Hex ChainId: {wallet.chainId}</div>
+          <div>Numeric ChainId: {formatChainAsNum(wallet.chainId)}</div>
+        </>
+      )}
     </div>
   );
 };
